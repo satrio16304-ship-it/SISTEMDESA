8cb7cb3e8168a9181f7f203bb7a371d5846202c5
satrio16304-ship-it <satrio16304@gmail.com>
2026-05-07 01:39:13 +0800

    Add files via upload

---
+++ config/koneksi.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "kependudukan_desa";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
+
+mysqli_set_charset($conn, "utf8mb4");
+
+?>
